3780f728bf48ce446fb403e4a1fa4b72591fdae8
moben8 <82767837+moben8@users.noreply.github.com>
2022-04-29 21:58:13 +0100

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1651264507,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"assets/images/player-sheet0.png",
+		"assets/images/tiledbackground.png",
+		"assets/images/bullet-sheet0.png",
+		"assets/images/enemy-sheet0.png",
+		"assets/images/bullet2-sheet0.png",
+		"assets/images/bullet3-sheet0.png",
+		"assets/images/bullet4-sheet0.png",
+		"assets/images/bullet5-sheet0.png",
+		"assets/images/trap-sheet0.png",
+		"assets/bullet-3.m4a",
+		"assets/bullet-3.ogg",
+		"assets/laser-shot.m4a",
+		"assets/laser-shot.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
